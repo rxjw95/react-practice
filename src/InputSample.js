10d3a0f08dddd9eb05a55872c1259fd4ef6d397e
Jangwook Ryu <rxjw95@gmail.com>
2021-07-19 16:58:10 +0900

    여러개의 input 상태 관리하기, ES6(spread, computed property)

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,22 +1,48 @@
 import React, { useState } from "react";
 
 function InputSample() {
-  const [value, setValue] = useState("");
+  const [inputs, setInputs] = useState({
+    name: "",
+    nickname: "",
+  });
+
+  const { name, nickname } = inputs;
+
+  /* 
+  객체 업데이트에 대한 주의, 객체를 새롭게 만들어서 변경해야한다. 
+  불변성을 지켜야한다. (react에서 가장 중요한 원칙)
+  */
 
   const onChange = (e) => {
-    setValue(e.target.value);
+    const { name, value } = e.target;
+
+    /*아래는 es6의 spread, computed property 문법이 사용되었다. 참고하자 */
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
   };
 
   const onReset = () => {
-    setValue("");
+    setInputs({
+      name: "",
+      nickname: "",
+    });
   };
+
   return (
     <div>
-      <input onChange={onChange} value={value} />
+      <input name="name" placeholder="이름" onChange={onChange} value={name} />
+      <input
+        name="nickname"
+        placeholder="별명"
+        onChange={onChange}
+        value={nickname}
+      />
       <button onClick={onReset}>초기화</button>
       <div>
         <b> 값 </b>
-        {value}
+        {name} ({nickname})
       </div>
     </div>
   );
