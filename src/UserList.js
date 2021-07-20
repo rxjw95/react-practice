32d473ffc90c800d8c72258cbf5af5b852c826ad
Jangwook Ryu <rxjw95@gmail.com>
2021-07-20 20:24:32 +0900

    map을 활용한 배열 렌더링 및 key 값 생성

--- src/UserList.js
+++ src/UserList.js
@@ -21,10 +21,14 @@ function UserList() {
   ];
 
   return (
+    /* 
+      map에서 컴포넌트 배열을 만들 때 key값을 사용하는 이유와 
+      key값을 user.id와 map의 두번째 param인 index를 사용하는 방식의 차이 알기
+    */
     <div>
-      <User user={users[0]} />
-      <User user={users[1]} />
-      <User user={users[2]} />
+      {users.map((user) => (
+        <User user={user} key={user.id} />
+      ))}
     </div>
   );
 }
