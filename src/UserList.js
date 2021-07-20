376be08a823bdee4977651808e5ca0491f022cd4
Jangwook Ryu <rxjw95@gmail.com>
2021-07-20 20:17:05 +0900

    두번째 불편한 배열 렌더링

--- src/UserList.js
+++ src/UserList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import User from "./User";
 
 function UserList() {
   const users = [
@@ -21,15 +22,9 @@ function UserList() {
 
   return (
     <div>
-      <div>
-        <b>{users[0].username}</b> <span>{users[0].email}</span>
-      </div>
-      <div>
-        <b>{users[1].username}</b> <span>{users[1].email}</span>
-      </div>
-      <div>
-        <b>{users[2].username}</b> <span>{users[2].email}</span>
-      </div>
+      <User user={users[0]} />
+      <User user={users[1]} />
+      <User user={users[2]} />
     </div>
   );
 }
