ad4861851442906ddd2af3fb66461e4a8dad95a6
Jangwook Ryu <rxjw95@gmail.com>
2021-08-10 13:23:19 +0900

    useEffect() 다뤄보기

--- src/User.js
+++ src/User.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function User({ user, onRemove, onToggle }) {
     const styleForm = {
@@ -8,6 +8,34 @@ function User({ user, onRemove, onToggle }) {
         borderRadius: "1rem",
     };
     const { id, username, email, active } = user;
+
+    useEffect(() => {
+        //렌더링된 직후, dom에 직접적인 접근이 가능하다.
+        // restAPI, props => state
+        console.log("component mounted");
+
+        //정리 함수, 라이브러리 인스턴스 제거
+        return () => {
+            console.log("componet unmounted");
+        };
+    });
+    /*
+    2번째 param은 dependency array
+    
+    빈 배열을 등록하면 컴포넌트가 렌더링할 때 한번만 호출된다.
+    
+    배열 내에 값을 등록하거나 해당 값이 설정되거나 업데이트 할 때마다 useEffect 실행된다.
+    
+    * 배열 자체를 넣지 않는 경우는 그저 컴포넌트가 리렌더링 될때마다 useEffect 함수가 호출된다.
+    헷갈리지 않게 하나하나 설명해보자.
+
+    1. inputs를 state로 관리하기 때문에 해당 값이 바뀌면 컴포넌트가 리렌더링될 것이고,
+     그에 따라 자식 컴포넌트들도 리렌더링되기 때문에 UserList, User 컴포넌트들도 리렌더링된다.
+    결국 리렌더링 될때마다 useEffect가 실행된다.
+
+    2. TOGGLE, REMOVE 시에 USER 상태가 바뀌기 때문에 상위에 user를 state/props로 가지고 있는 컴포넌트 리렌더링될 것이고,
+    그에 따라 user 컴포넌트도 리렌더링, 전부 useEffect 수행
+    */
 
     return (
         <div style={styleForm}>
